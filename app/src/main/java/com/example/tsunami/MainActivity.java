468ab34f76e0ed4ef5f617db571b412edf6bfba9
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-05 11:25:25 +0530

    JSON parsing done commit

---
+++ app/src/main/java/com/example/tsunami/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.tsunami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+
+
+
+    /** URL to query the USGS dataset for earthquake information */
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+        /**
+         * Return an {@link Event} object by parsing out information
+         * about the first earthquake from the input earthquakeJSON string.
+         */
+        private Event extractFeatureFromJson(String earthquakeJSON) {
+            try {
+                JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+                JSONArray featureArray = baseJsonResponse.getJSONArray("features");
+
+                // If there are results in the features array
+                if (featureArray.length() > 0) {
+                    // Extract out the first feature (which is an earthquake)
+                    JSONObject firstFeature = featureArray.getJSONObject(0);
+                    JSONObject properties = firstFeature.getJSONObject("properties");
+
+                    // Extract out the title, time, and tsunami values
+                    String title = properties.getString("title");
+                    long time = properties.getLong("time");
+                    int tsunamiAlert = properties.getInt("tsunami");
+
+                    // Create a new {@link Event} object
+                    return new Event(title, time, tsunamiAlert);
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+            }
+            return null;
+        }
+    }
